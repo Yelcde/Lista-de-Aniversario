3f0a9d5d9347ae9d3dfd7f443c98bb27e34fa59a
Johnner Yelcde <johnnercastrojy@gmail.com>
2023-04-05 21:14:06 -0300

    Projeto Lista de aniversariantes
    Finalizando o grosso do projeto lista de aniversariantes para a minha igreja. O projeto mostra, preechendo automaticamente com js o html, a lista de todos os aniversariantes.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,30 @@
-import aniversariantes from './model/aniversariantes.js';
+import { meses, aniversariantes } from './model/aniversariantes.js';
 
-const aniversariantesHTML = document.querySelector(".aniv")
+const mesesHTML = document.querySelector('.meses')
 
-function displayAniversariantes(flag)
-{
-    aniversariantesHTML.innerHTML += `
-    <p>${flag.data} - ${flag.nome}</p>
+for (const mes in meses) {
+    let aniversariantesDoMes = []
+    let aniversariantesDoMesStr = ''
+
+    for (const aniversariante of aniversariantes) {
+        if (aniversariante.mes == Number(mes) + 1) {
+            aniversariantesDoMes.push(`<p>${aniversariante.data} - ${aniversariante.nome}</p>`) 
+        }
+    }
+
+    aniversariantesDoMes.sort()
+
+    for (const i in aniversariantesDoMes){
+        aniversariantesDoMesStr += aniversariantesDoMes[i]
+    }
+
+    console.log(aniversariantesDoMes)
+
+    mesesHTML.innerHTML += `
+        <div class="content">
+            <h2>${meses[mes]}</h2>
+            <hr>
+            <div class="aniversariantes">${aniversariantesDoMesStr}</div>
+        </div>
     `
 }
-
-aniversariantes.forEach(displayAniversariantes)
-
-// aniversariantesHTML.innerHTML += `
-    // <div class="flag col-2 my-2 text-center">
-    //     <img src="${flag.image}" alt="${flag.name}">
-    //     <p>${flag.name}</p>
-    // </div> 
-    // `
--- js/model/aniversariantes.js
+++ js/model/aniversariantes.js
@@ -1,12 +1,220 @@
-const aniversariantes = [
+export const aniversariantes = [
   {
-    nome: 'Johnner',
+    nome: 'Viviane',
+    data: '20',
+    mes: 1,
+  },
+  {
+    nome: 'Álisson',
+    data: '31',
+    mes: 1,
+  },
+  {
+    nome: 'Claudécio',
+    data: '09',
+    mes: 2,
+  },
+  {
+    nome: 'Josué',
+    data: '23',
+    mes: 2,
+  },
+  {
+    nome: 'Pedro',
+    data: '28',
+    mes: 2,
+  },
+  {
+    nome: 'Eunice',
+    data: '09',
+    mes: 3
+  },
+  {
+    nome: 'Gerimar S.',
     data: '10',
+    mes: 3,
+  },
+  {
+    nome: 'Laudicéia',
+    data: '02',
+    mes: 4,
+  },
+  {
+    nome: 'Diogo',
+    data: '09',
+    mes: 4,
+  },
+  {
+    nome: 'Antônio',
+    data: '10',
+    mes: 4,
+  },
+  {
+    nome: 'Leila',
+    data: '19',
+    mes: 4,
   },
   {
     nome: 'Laucynéia',
-    data: '22',
+    data: '26',
+    mes: 4,
+  },
+  {
+    nome: 'Pr. Gerimar',
+    data: '30',
+    mes: 4,
+  },
+  {
+    nome: 'Oziel',
+    data: '02',
+    mes: 5,
+  },
+  {
+    nome: 'George',
+    data: '03',
+    mes: 5,
+  },
+  {
+    nome: 'Fabiana',
+    data: '07',
+    mes: 5,
+  },
+  {
+    nome: 'Ednaldo',
+    data: '11',
+    mes: 5,
+  },
+  {
+    nome: 'Érica',
+    data: '17',
+    mes: 5,
+  },
+  {
+    nome: 'Lêdson',
+    data: '31',
+    mes: 5,
+  },
+  {
+    nome: 'Ana Vitória',
+    data: '13',
+    mes: 6,
+  },
+  {
+    nome: 'Mikeyas',
+    data: '09',
+    mes: 7,
+  },
+  {
+    nome: 'Heitor',
+    data: '27',
+    mes: 7,
+  },
+  {
+    nome: 'Luzia',
+    data: '05',
+    mes: 8,
+  },
+  {
+    nome: 'Geisa',
+    data: '11',
+    mes: 8,
+  },
+  {
+    nome: 'Mirella',
+    data: '18',
+    mes: 8,
+  },
+  {
+    nome: 'Alcilene',
+    data: '28',
+    mes: 8,
+  },
+  {
+    nome: 'Édley K.',
+    data: '02',
+    mes: 9,
+  },
+  {
+    nome: 'Fernandes',
+    data: '07',
+    mes: 9,
+  },
+  {
+    nome: 'Johnner',
+    data: '10',
+    mes: 10,
+  },
+  {
+    nome: 'Tiago',
+    data: '16',
+    mes: 10,
+  },
+  {
+    nome: 'Heloisa',
+    data: '19',
+    mes: 10,
+  },
+  {
+    nome: 'André',
+    data: '27',
+    mes: 10,
+  },
+  {
+    nome: 'Edcley',
+    data: '29',
+    mes: 10,
+  },
+  {
+    nome: 'Nyedja',
+    data: '31',
+    mes: 10,
+  },
+  {
+    nome: 'Calebe',
+    data: '09',
+    mes: 11,
+  },
+  {
+    nome: 'Evaneide',
+    data: '18',
+    mes: 11,
+  },
+  {
+    nome: 'Mônica',
+    data: '18',
+    mes: 11,
+  },
+  {
+    nome: 'Hadrian',
+    data: '26',
+    mes: 11,
+  },
+  {
+    nome: 'Geisiane',
+    data: '29',
+    mes: 11,
+  },
+  {
+    nome: 'Rosilda',
+    data: '15',
+    mes: 12,
+  },
+  {
+    nome: 'Aliakmos',
+    data: '13',
+    mes: 12,
+  },
+  {
+    nome: 'Ester',
+    data: '05',
+    mes: 3,
+  },
+  {
+    nome: 'Pr. Assis',
+    data: '26',
+    mes: 3,
   },
 ];
 
-export default aniversariantes;
+
+export const meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
